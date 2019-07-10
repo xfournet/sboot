f61aec7685ed851afead74eded8ad3a1b52b47e1
Xavier Fournet <461943+xfournet@users.noreply.github.com>
2019-07-10 23:58:18 +0200

    Profiles loading timings are now available in $sboot_load_profiles_timing

--- bin/load-profiles.ps1
+++ bin/load-profiles.ps1
@@ -1,1 +1,8 @@
-Get-ChildItem -Filter "*.ps1" "$PSScriptRoot\..\profile" | ForEach-Object { . $_.FullName }
+$sboot_load_profiles_timing = @()
+Get-ChildItem -Filter "*.ps1" "$PSScriptRoot\..\profile" | ForEach-Object {
+    $stopwatch = New-Object System.Diagnostics.Stopwatch
+    $stopwatch.Start()
+    . $_.FullName
+    $stopwatch.Stop()
+    $sboot_load_profiles_timing += "Profile $($_.Name) took $($stopwatch.ElapsedMilliseconds) ms"
+}
