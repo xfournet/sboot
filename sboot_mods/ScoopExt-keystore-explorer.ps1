90b012c9fc837274900a5f4c88c9431163f939ff
Xavier Fournet <461943+xfournet@users.noreply.github.com>
2019-08-13 23:44:17 +0200

    Fix JRE path resolution for Keystore Explorer configuration

--- sboot_mods/ScoopExt-keystore-explorer.ps1
+++ sboot_mods/ScoopExt-keystore-explorer.ps1
@@ -3,7 +3,7 @@
 Function AppInstalled([String]$AppDir) {
     # keystore-explore required to have a jre directory in it's application directory in order to start
     $kseJreDir = "$AppDir\jre"
-    $javaHome = Split-Path -Parent (Split-Path -Parent "$( where.exe java.exe )")
+    $javaHome = Split-Path -Parent (Split-Path -Parent (Get-Command java.exe).Path)
     if ($javaHome) {
         EnsureJunction -JunctionPath $kseJreDir -TargetPath $javaHome
     } else {
