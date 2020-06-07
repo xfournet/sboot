e780d148e9574bbc2951d71dfdf6f9cbb682838c
Xavier Fournet <461943+xfournet@users.noreply.github.com>
2020-06-07 20:14:37 +0200

    Temporary fix of windows-terminal icons, waiting for https://github.com/lukesampson/scoop-extras/pull/4233 to be integrated

--- sboot_mods/ScoopExt-windows-terminal.ps1
+++ sboot_mods/ScoopExt-windows-terminal.ps1
@@ -5,6 +5,17 @@ Function AppInstalled([String]$AppDir) {
     if (Test-Path $persistSettings) {
         EnsureLink -LinkPath "$env:LOCALAPPDATA\Microsoft\Windows Terminal\settings.json" -TargetPath $persistSettings
     }
+
+    # Temporary fix of windows-terminal icons, waiting for https://github.com/lukesampson/scoop-extras/pull/4233 to be integrated
+    Get-ChildItem "$AppDir\\ProfileIcons" '*.png' | ForEach-Object {
+        $currentName = $_.Name
+        $newName = $_.Name.Replace('%7B', '{').Replace('%7D', '}')
+        if ($newName -ne $currentName) {
+            DoUpdate "Fix Windows terminal icon name '$currentName', renamed to '$newName'" {
+                $_ | Rename-Item -NewName $newName
+            }
+        }
+    }
 }
 
 Function AppUninstalled {
